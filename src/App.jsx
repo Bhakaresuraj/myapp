416d94ac82b4853e89ed68e44855cb3fb5597e13
Bhakaresuraj <surajbhakare190@gmail.com>
2026-01-22 14:37:59 +0530

    learned how to add dynamic styling on the basis of conditions

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,24 @@ import Product from './Product.jsx'
 let name = "suraj";
 function App() {
 
-  let arr=["h1","h2","h3"];
-  const obj={
-    name:"suraj",
-    age:20,
-    gen :"Male"
+  let arr = ["h1", "h2", "h3"];
+  const obj = {
+    name: "suraj",
+    age: 20,
+    gen: "Male"
   }
   return (
     <>
       <p>hello ,{name}</p>
       <p>Goog game {name}</p>
-      < Product title="phone" price="20000" position={arr} object={obj}/>
-      < Product title="mobile" price="70000"position={arr} object={obj}/>
-      < Product title="tv" price="35000"position={arr}object={obj}/>
-      < Product title="laptop" price="45000"position={arr} object={obj}/>
+      < Product title="phone" price="20000" position={["h1", "h2", "h3"]} object={{
+        name: "suraj",
+        age: 20,
+        gen: "Male"
+      }} />
+      < Product title="mobile" price="70000" position={arr} object={obj} />
+      < Product title="tv" price="35000" position={arr} object={obj} />
+      < Product title="laptop" price="45000" position={arr} object={obj} />
     </>
   )
 }
--- src/Product.jsx
+++ src/Product.jsx
@@ -1,17 +1,16 @@
 import "./Product.css";
 
 function Product({ title, price, position, object }) {
-    console.log(position);
+    // console.log(position);
+    let style = {
+        // backgroundColor: price >= 40000 ? "purple" : ""
+        backgroundColor:"aqua"
+    };
     return (
-        <div className="Product">
-            <h1>Hello</h1>
-
-            <p>hi this is {title}</p>
+        <div className="Product" >
+            <h1>{title}</h1>
             <p>having price {price}</p>
-            <p>{position}</p>
-            <p>{object.name}</p>
-            <p>{object.age}</p>
-            <p>{object.gen}</p>
+            {price >= 30000 && <p style={style}>Discount :10%</p>}
         </div>
     )
 }
