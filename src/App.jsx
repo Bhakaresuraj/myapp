0362cb424a62052d70e365c5aea5bd1ca1688c2e
Bhakaresuraj <surajbhakare190@gmail.com>
2026-01-22 08:42:43 +0530

    Learning react as frame work for front end

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+let name = prompt("Enter your name :");
+function App() {
+  return (
+    <>
+      <p>hello ,{name}</p>
+      <p>Goog game {name}</p>
+    </>
+  )
+}
+export default App
