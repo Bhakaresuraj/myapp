248aaf25fa4bc063d1b5c83e45dd5d21da2b8217
Bhakaresuraj <surajbhakare190@gmail.com>
2026-01-22 11:03:22 +0530

    learning props in react js

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import './App.css'
+import './App.css';
 import Product from './Product.jsx'
 let name = "suraj";
 function App() {
+
+  let arr=["h1","h2","h3"];
+  const obj={
+    name:"suraj",
+    age:20,
+    gen :"Male"
+  }
   return (
     <>
       <p>hello ,{name}</p>
       <p>Goog game {name}</p>
-      < Product />
-    </>git
+      < Product title="phone" price="20000" position={arr} object={obj}/>
+      < Product title="mobile" price="70000"position={arr} object={obj}/>
+      < Product title="tv" price="35000"position={arr}object={obj}/>
+      < Product title="laptop" price="45000"position={arr} object={obj}/>
+    </>
   )
 }
 export default App
- 
--- src/Product.jsx
+++ src/Product.jsx
@@ -1,16 +1,19 @@
+import "./Product.css";
 
-function Title() {
+function Product({ title, price, position, object }) {
+    console.log(position);
     return (
-        <>
+        <div className="Product">
             <h1>Hello</h1>
 
-            <p>hi this is .....</p>
-        </>
+            <p>hi this is {title}</p>
+            <p>having price {price}</p>
+            <p>{position}</p>
+            <p>{object.name}</p>
+            <p>{object.age}</p>
+            <p>{object.gen}</p>
+        </div>
     )
-}
-function Product() {
-    return <Title />
-        
 }
 
 export default Product;
